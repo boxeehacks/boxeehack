0dbe5727da10ad50d21b1bc22691f618cc6e8260
Daniël Haveman <daniel@utalk.nl>
2012-11-14 19:22:18 +0100

    Shell now has a password check to at least have a small bit of security. The password is "secret".

--- hack/shell.sh
+++ hack/shell.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+VALID_PASSWORD="secret"
+
 export LD_LIBRARY_PATH='.:/data/hack/lib:/opt/local/lib:/usr/local/lib:/usr/lib:/lib:/lib/gstreamer-0.10:/opt/local/lib/qt'
 export PATH='/data/hack/bin:/opt/local/bin:/usr/local/bin:/usr/bin:/bin:/opt/local/sbin:/usr/local/sbin:/usr/sbin:/sbin:/scripts'
-sh
+
+read -s -p "Password: " PASSWORD
+
+echo ""
+if [ "$PASSWORD" == "$VALID_PASSWORD" ]; then
+        echo "---------------------"
+        echo " Welcome to BoxeeBox "
+        echo "---------------------"
+        sh
+else
+        echo "Incorrect password"       
+fi
