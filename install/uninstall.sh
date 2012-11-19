20edf97502d50c5d7fe93e9ecd3e800c43ab58da
Daniël Haveman <daniel@utalk.nl>
2012-11-19 01:16:07 +0100

    Fixes an eternal loop issue on uninstall

--- install/uninstall.sh
+++ install/uninstall.sh
@@ -6,6 +6,8 @@ dtool 6 2 0 0
 
 # remove all traces of the install
 rm /data/.boxee/UserData/advancedsettings.xml
+/bin/busybox sed -i "s/;sh \/media\/BOXEE\/uninstall.sh//g" /data/etc/boxeehal.conf 
+/bin/busybox sed -i "s/; sh \/media\/BOXEE\/uninstall.sh//g" /data/etc/boxeehal.conf
 /bin/busybox sed -i "s/;sh \/data\/hack\/boot.sh//g" /data/etc/boxeehal.conf 
 /bin/busybox sed -i "s/; sh \/data\/hack\/boot.sh//g" /data/etc/boxeehal.conf 
 rm -Rf /data/hack
