cedfa9779b37c4b8910e3e5ecbef5e52bd2830fe
Daniël Haveman <daniel@utalk.nl>
2012-11-22 00:29:25 +0100

    Modified apps hook
    Added a hook for running modified apps
    Added modified boxee browser which points at google.com instead of bing

---
+++ hack/apps.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cp -R /data/hack/apps/browser2 /data/.boxee/UserData/apps/
--- hack/boot.sh
+++ hack/boot.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sh /data/hack/skin.sh &
 sh /data/hack/logo.sh &
+sh /data/hack/apps.sh &
 sh /data/hack/network.sh &
 sh /data/hack/telnet.sh &
 sh /data/hack/ftp.sh &
