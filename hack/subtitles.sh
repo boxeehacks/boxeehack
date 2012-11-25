d75b582eb2805a599539fa4777ed73d678baa8d9
Daniël Haveman <daniel@utalk.nl>
2012-11-25 01:28:14 +0100

    Subtitles plugin can now be enabled or disabled

--- hack/subtitles.sh
+++ hack/subtitles.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-mount -o bind /data/hack/boxee/scripts /opt/boxee/scripts
+SUBTITLES_ENABLED=`head -n 1 /data/etc/.subtitles_enabled`
+
+if [ "$SUBTITLES_ENABLED" == "1" ]; then
+
+	mount -o bind /data/hack/boxee/scripts/OpenSubtitles /opt/boxee/scripts/OpenSubtitles
+
+else
+
+	umount -f /opt/boxee/scripts/OpenSubtitles
+
+fi
