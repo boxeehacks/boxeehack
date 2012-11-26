87363a7934a8ce4800781c099587eb9f21e30d5c
jeroenb <jeroen@vdmi.nl>
2012-11-26 23:46:53 +0100

    Added tools category and item to clear thumbnail cache

---
+++ hack/boxee/skin/boxee/720p/scripts/boxeehack_clear_cache.py
@@ -0,0 +1,19 @@
+import os
+import xbmc, xbmcgui
+    
+def fanart_function():
+    dialog = xbmcgui.Dialog()
+    if dialog.yesno("Clear fanart cache", "Are you sure you want to clear the fanart cache?"):
+	pass
+
+def thumbnail_function():
+    dialog = xbmcgui.Dialog()
+    if dialog.yesno("Clear thumbnail cache", "Are you sure you want to clear the thumbnail cache?"):
+        os.system("find /data/.boxee/UserData/profiles/*/Thumbnails/ -name \*.tbn | xargs rm")
+
+if (__name__ == "__main__"):
+    section = sys.argv[1]
+    if section == "fanart":
+        fanart_function()
+    if section == "thumbnail":
+        thumbnail_function()
