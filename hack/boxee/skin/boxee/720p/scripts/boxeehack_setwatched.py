f58b94ecf86eaa228fcc9f79d551ac6992c2033f
jeroenb <jeroen@vdmi.nl>
2012-11-29 01:40:16 +0100

    Added a progress thingie

--- hack/boxee/skin/boxee/720p/scripts/boxeehack_setwatched.py
+++ hack/boxee/skin/boxee/720p/scripts/boxeehack_setwatched.py
@@ -12,10 +12,12 @@ def set_watched():
 	db_path = xbmc.translatePath('special://profile/Database/')
 	itemList = list.GetItems()
 	seasons = []
+	episodes_count = 0
 	for item in itemList:
 		season = item.GetSeason()
 		if(season != -1):
 			seasons.append(season)
+			episodes_count = episodes_count + 1
 
 	seasons = dict.fromkeys(seasons)
 	seasons = seasons.keys()
@@ -28,10 +30,19 @@ def set_watched():
 
 	dialog = xbmcgui.Dialog()
     	if dialog.yesno("Watched", "Do you want to mark all episodes of %s%s as watched?" % (series, season_string)):
+		progress = xbmcgui.DialogProgress()
+		progress.create('Updating episodes', 'Setting %s%s as watched' % (series, season_string))
+
+		current_count = 0
 		for item in itemList:
 			episode = item.GetEpisode()
 			if(episode != -1):
+				current_count = current_count+1
+				percent = int( ( episodes_count / current_count ) * 100)
+				message = "Episode " + str(current_count) + " out of " + str(episodes_count)
+				progress.update( percent, "", message, "" )
 				os.system('/data/hack/bin/sqlite3 ' + db_path + 'boxee_user_catalog.db \'INSERT INTO watched VALUES(null, "'+item.GetPath()+'", null, 1, 0, -1.0);\'')
+		progress.close()
 		mc.ShowDialogNotification('Marked all episodes for %s%s as watched!' % (series, season_string))
 
 if (__name__ == "__main__"):
