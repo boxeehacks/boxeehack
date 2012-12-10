8548895aadc0289d9ee7c8599561040c64b3e48a
boxeehacks <boxee@utalk.nl>
2012-12-10 21:30:31 +0100

    Fix (hopefully) for the VPN issues

--- hack/network.sh
+++ hack/network.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-ln -s /data/etc/ppp/secrets /data/hack/ppp/chap-secrets
-ln -s /data/etc/ppp/options.pptp /data/hack/ppp/options.pptp
-ln -s /data/etc/ppp/secrets /data/hack/ppp/pap-secrets
-ln -s /data/etc/ppp/resolv.conf /data/hack/ppp/resolv.conf
-ln -s /data/etc/ppp/peers/vpn /data/hack/ppp/peers/vpn
+mkdir /data/hack/ppp_mnt
+cp -R /etc/ppp/* /data/hack/ppp_mnt/
+cp -R /data/hack/ppp/* /data/hack/ppp_mnt/
 mount -o bind /data/hack/ppp /etc/ppp
