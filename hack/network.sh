74581af21ac2cafaf69fd77ed7e2a8d26930c558
boxeehacks <boxee@utalk.nl>
2012-12-11 11:13:33 +0100

    Finally fixes vpn access (for real!)

--- hack/network.sh
+++ hack/network.sh
@@ -3,4 +3,4 @@ chmod +x /data/hack/ppp/*
 mkdir /data/hack/ppp_mnt
 cp -R /etc/ppp/* /data/hack/ppp_mnt/
 cp -R /data/hack/ppp/* /data/hack/ppp_mnt/
-mount -o bind /data/hack/ppp /etc/ppp
+mount -o bind /data/hack/ppp_mnt /etc/ppp
