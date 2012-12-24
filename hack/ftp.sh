7471d6cc9a47119712dc7236546c9aa8629fee9e
boxeehacks <boxee@utalk.nl>
2012-12-24 19:22:57 +0100

    Updated ftp

--- hack/ftp.sh
+++ hack/ftp.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-mkdir /data/media
-mount -o bind /tmp/mnt /data/media
-nohup /data/hack/bin/busybox tcpsvd -vE 0.0.0.0 21 /data/hack/bin/busybox ftpd -w /data > /dev/null &
+nohup /data/hack/bin/busybox tcpsvd -vE 0.0.0.0 21 /data/hack/bin/busybox ftpd -w / > /dev/null &
 
