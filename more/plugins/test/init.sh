121efb55edaf22118cfc2b66644092470095dc15
boxeehacks <boxee@utalk.nl>
2012-12-24 19:22:41 +0100

    Sample plugin

--- more/plugins/test/init.sh
+++ more/plugins/test/init.sh
@@ -3,7 +3,7 @@
 BASEDIR=`dirname $0`
 
 if [ "$1" == "start" ]; then
-    chmod +x ${BASEDIR}/bin/test
+    chmod +x ${BASEDIR}/bin/test_plugin
 	echo "Test Plugin Started"
 elif [ "$1" == "stop" ]; then
 	echo "Test Plugin Stopped"
